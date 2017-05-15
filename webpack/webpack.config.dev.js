01d3aa9def805c25830e9138505f9301765d0fac
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-05-15 17:53:34 +0300

    Create projct sceleton

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,32 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ["./app/src/app.js"]
+  },
+  output: {
+    path: path.resolve("build"),
+    filename: "bundle.js"
+  },
+  devServer : {
+    contentBase : "build"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ],
+    loaders: []
+  },
+  plugins: [
+    new HtmlWebpackPlugin(({
+      title: 'DA-MI',
+      filename: 'index.html',
+      template: '</ttt>'
+    }))
+  ]
+};
