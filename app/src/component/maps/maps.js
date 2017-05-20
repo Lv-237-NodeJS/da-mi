317c4949968d00ccb7140da7d34df61d384960b4
Denis <denis.bodnar@gmail.com>
2017-05-20 15:28:23 +0300

    Add example components and make links

---
+++ app/src/component/maps/maps.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Maps extends Component {
+  render(){
+    return(
+			<div>
+				<h2>Find us on maps</h2>
+				<p>You can look for our location on Google Maps</p>
+			</div>
+    );
+  }
+}
+
+export default Maps;
