998e044c903655be1715be9922a83db74a1c9785
Denis <denis.bodnar@gmail.com>
2017-05-20 16:15:53 +0300

    Fix whitespaces

--- app/src/component/maps/maps.js
+++ app/src/component/maps/maps.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Maps extends Component {
   render(){
     return(
-			<div>
-				<h2>Find us on maps</h2>
-				<p>You can look for our location on Google Maps</p>
-			</div>
+      <div>
+        <h2>Find us on maps</h2>
+        <p>You can look for our location on Google Maps</p>
+      </div>
     );
   }
 }
