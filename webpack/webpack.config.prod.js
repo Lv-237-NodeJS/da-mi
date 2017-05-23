902981fdc9cf531d99c3a9c8d922b3cd55337ba0
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-05-23 19:40:12 +0300

    Deleted deprecated loader

--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,23 +1,15 @@
-const WebpackStripLoader = require('strip-loader');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-let devConfig = require('./webpack.config.dev.js');
+const cleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 const rootPath = path.resolve('./');
 const buildPath = path.resolve('build');
+let devConfig = require('./webpack.config.dev.js');
 
-let stripLoader = {
-	test: [/\.js$/],
-	exclude: /node_modules/,
-	use: ['strip-loader']
-};
-
-let cleanBuildplagin = new CleanWebpackPlugin([buildPath], {
+let cleanBuildplagin = new cleanWebpackPlugin([buildPath], {
   root: rootPath,
   verbose: true,
   dry: false
 });
 
-devConfig.module.loaders.push(stripLoader);
 devConfig.plugins.push(cleanBuildplagin);
 
 module.exports = devConfig;
