fb07ca41e527f14dbf71ceb2bd3c4d43066532d5
Denis <denis.bodnar@gmail.com>
2017-05-24 22:44:04 +0300

    Rename containers folder

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './container';
+import { App } from './containers';
 
 const app = document.getElementById('root');
 
