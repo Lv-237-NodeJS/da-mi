eae13fabed738d37aa51ba194e18df22e9a3a733
Denis <denis.bodnar@gmail.com>
2017-05-24 08:39:58 +0300

    Add simple routing

---
+++ app/src/router/router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+import { About, Home, Contacts, Main, Address, Maps } from '../components';
+
+const routes = (
+  <Route path='/' component={Main}>
+    <IndexRoute component={Home}/>
+    <Route path='about' component={About}/>
+    <Route path='contacts' component={Contacts}/>
+  </Route>
+);
+
+export default routes;
