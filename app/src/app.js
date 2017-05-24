f119cf3d4f411b841f3dd212b0e361c5613f300f
Denis <denis.bodnar@gmail.com>
2017-05-24 08:50:50 +0300

    Add working routing

--- app/src/app.js
+++ app/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
-import routes from './router/routes';
+import routes from './router/router';
 
 ReactDOM.render(
   <Router history={browserHistory} routes={routes}/>,
