7d353d210024d53812d2afec6aca0e3a937c6690
Denis <denis.bodnar@gmail.com>
2017-05-24 08:42:21 +0300

    Add routes rendering to app.js

--- app/src/app.js
+++ app/src/app.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Hello } from './components';
+import { Router, browserHistory } from 'react-router';
+import routes from './router/routes';
 
-ReactDOM.render(<Hello/>, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={browserHistory} routes={routes}/>,
+  document.getElementById('root')
+);
