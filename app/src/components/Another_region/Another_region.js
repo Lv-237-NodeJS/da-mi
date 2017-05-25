a9380f2439941b1593b014d8412f40f78d4ac886
levanwork <levanwork@ukr.net>
2017-05-25 15:28:01 +0300

    squash 8 commits

--- app/src/components/Another_region/Another_region.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default class Another_region extends React.Component {
-  render() {
-    return <h1>You can find us in Another_region</h1>;
-  }
-}
--- app/src/router.js
+++ app/src/router.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { Route, IndexRoute, Link } from 'react-router';
-import { About, Address, Another_region, Home, Contacts, Main, Maps } from './components';
+import { Route, IndexRoute } from 'react-router';
+import { About, Address, Home, Contacts, Main, Maps } from './containers';
 
 const Routes = (
   <Route path='/' component={Main}>
     <IndexRoute component={Home}/>
     <Route path='about' component={About}/>
     <Route path='contacts' component={Contacts}>
-    	<Route path='address' component={Address}>
-    		<Route path='/contacts/address/another_region' component={Another_region}/>
-    	</Route>
-    	<Route path='maps' component={Maps}/>
+      <Route path='address' component={Address}/>
+      <Route path='maps' component={Maps}/>
     </Route>
   </Route>
 );
