f0739402d1896d1f34fa5cee6fe3e2954eee9d10
Denis <denis.bodnar@gmail.com>
2017-05-25 08:41:58 +0300

    Correct export statements

--- app/src/router/router.js
+++ app/src/router/router.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import { About, Home, Contacts, Main } from '../components';
 
-const Routes = (
+export default Routes = (
   <Route path='/' component={Main}>
     <IndexRoute component={Home}/>
     <Route path='about' component={About}/>
     <Route path='contacts' component={Contacts}/>
   </Route>
 );
-
-export default Routes;
