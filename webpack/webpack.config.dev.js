e57a535b70456dff0ec817ce52cd8a0eeeb2b31f
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-05-27 13:39:54 +0300

    Connected bootstrap sass

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -6,7 +6,7 @@ const entryPath = './app/src/app.js';
 
 module.exports = {
   entry: {
-    app: entryPath
+    app: ['bootstrap-loader', entryPath]
   },
   output: {
     path: path.resolve('./'),
@@ -28,7 +28,11 @@ module.exports = {
           fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
         })
-      }
+      },
+      {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, use: 'file-loader'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, use: 'url-loader?limit=10000&mimetype=image/svg+xml'}
     ]
   },
   plugins: [
@@ -37,10 +41,7 @@ module.exports = {
       filename: 'index.html',
       inject: false,
       template: require('html-webpack-template'),
-      appMountId: 'root',
-      links: [
-        'https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css'
-      ]
+      appMountId: 'root'
     }),
     new extractTextPlugin('bundle.css'),
     new webpack.EnvironmentPlugin({'NODE_ENV': 'development'})
