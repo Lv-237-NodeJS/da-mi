187a498f9d1fbb386f811a5a5eb015fe0b4185f8
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-05-30 02:40:15 +0300

    Moved from components to containers

--- app/src/app.js
+++ app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './containers';
+import App from './containers/App/App';
 
 const app = document.getElementById('root');
 
