462d6f2ca0facb2e8232774ec4719228d4ecb420
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-06-01 00:18:20 +0300

    Form refactoring

--- app/src/components/Login/Login.js
+++ app/src/components/Login/Login.js
@@ -2,6 +2,17 @@ import React from 'react';
 import { Form, FormGroup, ControlLabel, FormControl, Col, Button }
 from 'react-bootstrap';
 
+let InputGroup = ({id, label, ...props}) => (
+  <FormGroup controlId={id}>
+    <Col componentClass={ControlLabel} sm={12} md={3}>
+      {label}
+    </Col>
+    <Col sm={12} md={9}>
+      <FormControl {...props} required/>
+    </Col>
+  </FormGroup>
+);
+
 export default class Login extends React.Component {
   constructor (props) {
     super(props);
@@ -23,31 +34,22 @@ export default class Login extends React.Component {
   render () {
     return (
       <Form horizontal onSubmit={this.handleSubmit}>
-        <FormGroup controlId="email">
-          <Col componentClass={ControlLabel} sm={12} md={3}>
-            Email
-          </Col>
-          <Col sm={12} md={9}>
-            <FormControl type="email"
-                         placeholder="Email"
-                         value={this.state.email}
-                         onChange={this.handleChange('email')}
-                         required />
-          </Col>
-        </FormGroup>
-        <FormGroup controlId="password">
-          <Col componentClass={ControlLabel} sm={12} md={3}>
-            Password
-          </Col>
-          <Col sm={12} md={9}>
-            <FormControl type="password"
-                         placeholder="Password"
-                         value={this.state.password}
-                         onChange={this.handleChange('password')}
-                         required />
-          </Col>
-        </FormGroup>
-
+        <InputGroup
+          id="email"
+          label="Email"
+          type="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={this.handleChange('email')}
+        />
+        <InputGroup
+          id="password"
+          label="Password"
+          type="password"
+          placeholder="Password"
+          value={this.state.password}
+          onChange={this.handleChange('password')}
+        />
         <FormGroup>
           <Col smOffset={0} sm={12} mdOffset={3} md={9}>
             <Button type="submit" bsStyle="primary" bsSize="large">
