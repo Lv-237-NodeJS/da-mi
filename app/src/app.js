d99369a9398e39218e43900c633ff020adf6fdba
Denis <denis.bodnar@gmail.com>
2017-06-02 11:08:38 +0300

    Add redux-router-reducer

--- app/src/app.js
+++ app/src/app.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './containers/App';
-import configureStore from './store/configureStore';
-
-const store = configureStore();
 
 const app = document.getElementById('root');
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   app
 );
--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import Routes from './../router';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import Routes from './../router';
+import configureStore from './../store/configureStore';
+
+const store = configureStore();
+const history = syncHistoryWithStore(browserHistory, store);
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router history={browserHistory} routes={Routes} />
+      <Provider store={store}>
+        <Router history={history} routes={Routes} />
+      </Provider>
     );
   }
 };
