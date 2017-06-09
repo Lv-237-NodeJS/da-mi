f4cf11ac4c8e087c7e8b1d04b59c8eb9ffdf4085
Ivan Shyika <i.shyyka@ukr.net>
2017-06-09 04:56:28 +0300

    Add reducers and actions

--- app/src/containers/Event/EventDetails.js
+++ app/src/containers/Event/EventDetails.js
@@ -3,23 +3,32 @@ import { Link } from 'react-router';
 import { Grid, Row, Col, PageHeader, } from 'react-bootstrap';
 import { Nav, NavItem } from 'react-bootstrap';
 
-export default class Event extends React.Component {
+class EventDetails extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+  }
+
+  componentDidMount() {
+    this.props.fetchEventById(this.props.params.id);
+  }
 
   render() {
-    const events = this.props.response.data;
-    const id = this.props.params.id;
-    const event = events.filter(event => {
-      if (event.id == id) {
-        return event;
-      }
-    });
+    // const events = this.props.response.data;
+    // const id = this.props.params.id;
+    // const event = events.filter(event => {
+    //   if (event.id == id) {
+    //     return event;
+    //   }
+    // });
+
+    event = this.props.event;
 
     return (
       <Grid>
         <Row className="show-grid">
           <Col sm={12} md={4}>
 
-          <PageHeader className="text-center">Menu</PageHeader>
+          <PageHeader className="text-center"> Menu </PageHeader>
             <Nav bsStyle="pills" stacked >
               <Link className="list-group-item" to={'/events/' + id + '/guests'}>
                   Guests
@@ -34,15 +43,15 @@ export default class Event extends React.Component {
           </Col>
 
           <Col sm={12} md={8}>
-          <PageHeader className="text-center">{event[0].name}</PageHeader>
+          <PageHeader className="text-center"> { event.name } </PageHeader>
             <div>
                 <h1>Details: </h1>
-                <p><strong>Place </strong>: {event[0].location_name}</p>
-                <p><strong>Date </strong>: {event[0].date_event}</p>
-                <p><strong>Description</strong>: {event[0].description}</p>
+                <p><strong> Date </strong>: { event.date_event } </p>
+                <p><strong> Place </strong>: { event.location_name } </p>
+                <p><strong> Description </strong>: { event.description } </p>
             </div>
-            
-           {this.props.children}
+
+           { this.props.children }
 
           </Col>
         </Row>
@@ -50,3 +59,18 @@ export default class Event extends React.Component {
     );
   }
 };
+const mapStateToProps = (state, ownProps) => {
+  return {
+    event: state.event,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchEventById: eventId => dispatch(
+      EventActions.fetchEventById(eventId)
+  ),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventDetailPage);
--- app/src/redux/reducer.js
+++ app/src/redux/reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import items from './Test';
+import { eventsReducer, eventReducer } from './reducers/EventReducer';
 import { routerReducer } from 'react-router-redux';
 
 export default combineReducers({
   items,
-  routing: routerReducer
+  events: eventsReducer,
+  event: eventReducer,
+  routing: routerReducer,
 });
