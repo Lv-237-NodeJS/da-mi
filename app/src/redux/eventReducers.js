6b78ace08cdf3ddc0395898965db20b1c08eb5ee
Ivan Shyika <i.shyyka@ukr.net>
2017-06-12 00:56:58 +0300

    Remove unnecessary outputs

--- app/src/redux/eventReducers.js
+++ app/src/redux/eventReducers.js
@@ -1,63 +1,57 @@
 const FETCH_EVENTS = 'FETCH_EVENTS';
 const FETCH_EVENTS_FULFILLED = 'FETCH_EVENTS_FULFILLED';
 const FETCH_EVENTS_REJECTED = 'FETCH_EVENTS_REJECTED';
+const apiUrl = 'http://localhost:8082';
 
-// const FETCH_EVENT_BY_ID_SUCCESS = 'FETCH_EVENT_BY_ID_SUCCESS';
-//const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+import request from 'superagent';
 
-const apiUrl = 'http://localhost:8082/api/events';
-
-//import axios from "axios";
-import request from "superagent";
-
-export function fetchEvents(){
-  return function(dispatch){
+export function fetchEvents() {
+  return (dispatch) => {
     request
-    .get(apiUrl)
-    .end(function(err, res){
-      if(err){
+    .get(apiUrl + '/api/events')
+    .end((err, res) => {
+      if (err) {
         dispatch({
           type: FETCH_EVENTS_REJECTED,
-          payload: err 
-          })
-      } else {
-        //console.log('From superAgenta: ', res.body);      
-        dispatch({ 
-          type: FETCH_EVENTS_FULFILLED, 
-          payload: res.body
+          payload: err,
         });
-      }      
-    })
-  }
+      } else {
+        dispatch({
+          type: FETCH_EVENTS_FULFILLED,
+          payload: res.body,
+        });
+      }
+    });
+  };
 }
 
 export default function reducer(state={
-  events:[],
+  events: [],
   fetching: false,
   fetched: false,
-  error: null
-}, action){
+  error: null,
+}, action) {
   switch (action.type) {
     case FETCH_EVENTS: {
-      return {...state, fetching: true}
+      return { ...state, fetching: true };
     }
-    case FETCH_EVENTS_FULFILLED:{
-      //console.log('from reducer ----: ', action.payload);
+
+    case FETCH_EVENTS_FULFILLED: {
       return {
               ...state,
               fetching: false,
               fetched: true,
-              events: action.payload
-              }
+              events: action.payload,
+            };
     }
-     case FETCH_EVENTS_REJECTED: {
-       return {
-                ...state,
-                fetching: false,
-                error: action.payload
-              }
-     }
 
+    case FETCH_EVENTS_REJECTED: {
+      return {
+              ...state,
+              fetching: false,
+              error: action.payload,
+            };
+    }
   }
   return state;
 }
