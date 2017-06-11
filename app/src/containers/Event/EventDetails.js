1183995e360ef7e8e18d1874863682bf49622645
Ivan Shyika <i.shyyka@ukr.net>
2017-06-12 00:12:14 +0300

    Modified EventDetails component

--- app/src/containers/Event/EventDetails.js
+++ app/src/containers/Event/EventDetails.js
@@ -1,33 +1,8 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { Link } from 'react-router';
-import { Grid, Row, Nav, Col, Button, ButtonToolbar, PageHeader, } from 'react-bootstrap';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as eventActions from '../../redux/eventReducers';
+import { Grid, Row, Col, PageHeader, } from 'react-bootstrap';
+import { Nav, NavItem } from 'react-bootstrap';
 
-<<<<<<< f461c55d99e72e7e1ca6dc28fad4f0d949ee6977
-@connect( (state, ownProps) => ({ 
-  event: state.event
-  }), dispatch => ({  actions: bindActionCreators(eventActions, dispatch) }))
-
-export default class EventDetails extends React.Component {
-  constructor (props, context) {
-    super(props, context);
-    this.state = {
-      current: {},
-    };
-  }
-
-  componentWillMount() {
-    this.props.actions.fetchEventById(this.props.params.id);
-    this.setState({ event: this.props.event.current });
-  }
-
-  render() {
-
-    const id = this.props.params.id;
-    const event = this.props.event.current;
-=======
 class EventDetails extends React.Component {
   constructor(props, context) {
     super(props, context);
@@ -47,24 +22,11 @@ class EventDetails extends React.Component {
     // });
 
     event = this.props.event;
->>>>>>> Add reducers and actions
 
     return (
       <Grid>
         <Row className="show-grid">
           <Col sm={12} md={4}>
-<<<<<<< f461c55d99e72e7e1ca6dc28fad4f0d949ee6977
-          <PageHeader className="text-center"> Menu </PageHeader>
-            <Link className="list-group-item" to={'/events/' + id + '/guests'}>
-              Guests
-            </Link>
-            <Link className="list-group-item" to={'/events/' + id + '/gifts'}>
-              Gifts
-            </Link>
-            <Link className="list-group-item" to='/events'>
-              Back to Events
-            </Link>
-=======
 
           <PageHeader className="text-center"> Menu </PageHeader>
             <Nav bsStyle="pills" stacked >
@@ -78,40 +40,24 @@ class EventDetails extends React.Component {
                   Back to Events
                 </Link>
             </Nav>
->>>>>>> Add reducers and actions
           </Col>
+
           <Col sm={12} md={8}>
           <PageHeader className="text-center"> { event.name } </PageHeader>
-<<<<<<< f461c55d99e72e7e1ca6dc28fad4f0d949ee6977
-            <ButtonToolbar>
-              <Button bsStyle="primary"> Edit </Button>
-              <Button bsStyle="danger"> Delete </Button>
-            </ButtonToolbar>
-            <div>
-                <h3>Details: </h3>
-=======
             <div>
                 <h1>Details: </h1>
->>>>>>> Add reducers and actions
                 <p><strong> Date </strong>: { event.date_event } </p>
                 <p><strong> Place </strong>: { event.location_name } </p>
                 <p><strong> Description </strong>: { event.description } </p>
             </div>
-<<<<<<< f461c55d99e72e7e1ca6dc28fad4f0d949ee6977
-            { this.props.children }
-=======
 
            { this.props.children }
 
->>>>>>> Add reducers and actions
           </Col>
         </Row>
       </Grid>
     );
   }
-<<<<<<< f461c55d99e72e7e1ca6dc28fad4f0d949ee6977
-}
-=======
 };
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -128,4 +74,3 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventDetailPage);
->>>>>>> Add reducers and actions
--- app/src/redux/eventReducers.js
+++ app/src/redux/eventReducers.js
@@ -1,3 +1,4 @@
+<<<<<<< 32d27be48f03199280dc432a6e5eeff863fd252e
 const FETCH_EVENT_BY_ID = 'FETCH_EVENT_BY_ID';
 const FETCH_EVENT_BY_ID_FULFILLED = 'FETCH_EVENT_BY_ID_FULFILLED';
 const FETCH_EVENT_BY_ID_REJECTED = 'FETCH_EVENT_BY_ID_REJECTED';
@@ -59,3 +60,68 @@ export const eventReducers = (state = initialState, action) => {
 };
 
 export default eventReducers;
+=======
+const FETCH_EVENTS = 'FETCH_EVENTS';
+const FETCH_EVENTS_FULFILLED = 'FETCH_EVENTS_FULFILLED';
+const FETCH_EVENTS_REJECTED = 'FETCH_EVENTS_REJECTED';
+
+// const FETCH_EVENT_BY_ID_SUCCESS = 'FETCH_EVENT_BY_ID_SUCCESS';
+//const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+const apiUrl = 'http://localhost:8082/api/events';
+
+//import axios from "axios";
+import request from "superagent";
+
+export function fetchEvents(){
+  return function(dispatch){
+    request
+    .get(apiUrl)
+    .end(function(err, res){
+      if(err){
+        dispatch({
+          type: FETCH_EVENTS_REJECTED,
+          payload: err 
+          })
+      } else {
+        //console.log('From superAgenta: ', res.body);      
+        dispatch({ 
+          type: FETCH_EVENTS_FULFILLED, 
+          payload: res.body
+        });
+      }      
+    })
+  }
+}
+
+export default function reducer(state={
+  events:[],
+  fetching: false,
+  fetched: false,
+  error: null
+}, action){
+  switch (action.type) {
+    case FETCH_EVENTS: {
+      return {...state, fetching: true}
+    }
+    case FETCH_EVENTS_FULFILLED:{
+      //console.log('from reducer ----: ', action.payload);
+      return {
+              ...state,
+              fetching: false,
+              fetched: true,
+              events: action.payload
+              }
+    }
+     case FETCH_EVENTS_REJECTED: {
+       return {
+                ...state,
+                fetching: false,
+                error: action.payload
+              }
+     }
+
+  }
+  return state;
+}
+>>>>>>> Modified EventDetails component
