b9c2316064f2d36228a7d101fe9c068719f9b648
yuliannakorol <yuliannakorol@gmail.com>
2017-06-14 01:30:04 +0300

    created reducers for profile component

--- app/src/containers/Profile/Profile.js
+++ app/src/containers/Profile/Profile.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import request from 'superagent';
-import { API } from './../../helper/constants';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as profileActions from '../../redux/ProfileReducers';
 
-export default class Profile extends React.Component {
+class Profile extends React.Component {
 
-  constructor() {
-    super();
+  constructor(props, context) {
+    super(props, context);
+
     this.state = {
         firstName: null,
         lastName: null
@@ -13,26 +16,42 @@ export default class Profile extends React.Component {
   }
 
   componentWillMount() {
-    let userId = sessionStorage.getItem('userId')
-    request
-        .get(API.HOST + API.PORT + '/api/user/' + userId)
-        .accept('json')
-        .set('Content-Type', 'application/json')
-        .then((response) => {
-            this.setState({
-                firstName: response.body.firstName,
-                lastName: response.body.lastName,
-              });
-          });
+    this.props.actions.retrieveProfile(this.state.profile);
+    const data = this.props.profile;
+    this.setState({
+      firstName: data.profile.firstName,
+      lastName: data.profle.lastName,
+    });
+    // let userId = sessionStorage.getItem('userId')
+    // request
+    //     .get(API.HOST + API.PORT + '/api/user/' + userId)
+    //     .accept('json')
+    //     .set('Content-Type', 'application/json')
+    //     .then((response) => {
+    //         this.setState({
+    //             firstName: response.body.firstName,
+    //             lastName: response.body.lastName,
+    //           });
+    //       });
   };
 
   render() {
-    return (
-      <div>
-        <h1>Manage My Profile Data</h1>
-        <p>First Name: {this.state.firstName}</p>
-        <p>Last Name: {this.state.lastName}</p>
-    </div>
-  );
+    const profileNode = this.props.profile.profile.map((item) => {
+      return (
+        <div>
+          <h1>Manage My Profile Data</h1>
+            <p>First Name: {this.props.state.firstName}</p>
+            <p>Last Name: {this.props.state.lastName}</p>
+        </div>
+      );
+    });
   }
 }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(profileActions, dispatch),
+  };
+};
+
+export default connect(mapDispatchToProps, mapDispatchToProps)(Profile);
