504e83ae1dfc8fc1eb9a11d4503b1fc614ffe796
Ivan Shyika <i.shyyka@ukr.net>
2017-06-13 20:31:12 +0300

    Resolve conflict

--- app/src/redux/reducer.js
+++ app/src/redux/reducer.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux';
-
 import { routerReducer } from 'react-router-redux';
-import { eventsReducer, eventReducer } from './reducers/EventReducer';
 import items from './Test';
 import login from './Login';
 import event from './eventReducers';
@@ -9,8 +7,6 @@ import event from './eventReducers';
 export default combineReducers({
   items,
   login,
-  events: eventsReducer,
-  event: eventReducer,
   event,
   routing: routerReducer
 });
