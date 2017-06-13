16530555778f287295972282bfab30d111e11929
yuliannakorol <yuliannakorol@gmail.com>
2017-06-14 01:30:04 +0300

    created reducers for profile component

---
+++ app/src/redux/ProfileReducers.js
@@ -0,0 +1,67 @@
+import request from './../helper/request';
+import { API } from './../helper/constants';
+
+const RETRIEVE_PROFILE = 'RETRIEVE_PROFILE';
+const RETRIEVE_PROFILE_SUCCESS = 'RETRIEVE_PROFILE_SUCCESS';
+const RETRIEVE_PROFILE_FAILURE = 'RETRIEVE_PROFILE_FAILURE';
+
+export const retrieveProfile = () => {
+  let userId = sessionStorage.getItem('userId');
+  return (dispatch) => {
+    return request
+    .get(API.HOST + API.PORT + '/api/user/' + userId)
+    .end((err, res) => {
+      if (err) {
+        dispatch({
+          type: RETRIEVE_PROFILE_FAILURE,
+          payload: err,
+        });
+      } else {
+        dispatch({
+          type: RETRIEVE_PROFILE_SUCCESS,
+          payload: res.body
+        });
+      }
+    });
+  };
+};
+
+const initialState = {
+  profile: {
+    firstName: null,
+    lastName: null,
+  },
+  retrieving: false,
+  retrieved: false,
+  error: null
+};
+
+export const profileReducers = (state = initialState, action) => {
+  switch (action.type) {
+
+    case RETRIEVE_PROFILE: {
+      return {...state, retrieving: true};
+    }
+
+    case RETRIEVE_PROFILE_SUCCESS: {
+      return {
+        ...state,
+        retrieving: false,
+        retrieved: true,
+        profile: action.payload
+      };
+    }
+
+    case RETRIEVE_PROFILE_FAILURE: {
+      return {
+        ...state,
+        retrieving: false,
+        error: action.payload
+      };
+    }
+
+    default: return state;
+  }
+};
+
+export default profileReducers;
--- app/src/redux/reducer.js
+++ app/src/redux/reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import items from './Test';
 import login from './Login';
+import profile from './ProfileReducers';
 
 export default combineReducers({
   items,
+  profile,
   login,
   routing: routerReducer
 });
