544ef3b8d7b1e61bed4b3920dc706e1285350dd0
yuliannakorol <yuliannakorol@gmail.com>
2017-06-13 23:58:22 +0300

    updated requests helper with getting userid from session storage

--- app/src/helper/request.js
+++ app/src/helper/request.js
@@ -2,6 +2,7 @@ import defaults from 'superagent-defaults';
 
 const superagent = defaults();
 const token = sessionStorage.getItem('token');
+const userId = sessionStorage.getItem('userId');
 
 token && superagent.set('x-access-token', token);
 
