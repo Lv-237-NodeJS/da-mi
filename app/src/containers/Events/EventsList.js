5b97bbf0fcb67fd9ea823139dca12dbea4eadfe9
yuliannakorol <yuliannakorol@gmail.com>
2017-06-13 13:33:37 +0300

    fixed indentation

--- app/src/containers/Events/EventsList.js
+++ app/src/containers/Events/EventsList.js
@@ -8,34 +8,34 @@ export default class EventsList extends React.Component {
       <div>
         <h1>My Mock Events</h1>
         <Table>
-            <thead>
-             <tr>
-               <th>Name</th>
-               <th>Date</th>
-               <th>Place</th>
-               <th>Actions</th>
-             </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Toms Birthday Party</td>
-                    <td>06/29/2017  6:30 pm</td>
-                    <td>Lviv, Street 1, 22/2</td>
-                    <td> <Button>Accept</Button>  <Button>Decline</Button></td>
-                </tr>
-                <tr>
-                    <td>Wedding 1</td>
-                    <td>06/29/2017  6:30 pm</td>
-                    <td>Lviv, Street 1, 22/2</td>
-                    <td> <Button>Edit</Button>  <Button>Delete</Button></td>
-                </tr>
-                <tr>
-                    <td>Wedding 1</td>
-                    <td>06/29/2017  6:30 pm</td>
-                    <td>Lviv, Street 1, 22/2</td>
-                    <td> <Button>Edit</Button>  <Button>Delete</Button></td>
-                </tr>
-            </tbody>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date</th>
+              <th>Place</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Toms Birthday Party</td>
+              <td>06/29/2017  6:30 pm</td>
+              <td>Lviv, Street 1, 22/2</td>
+              <td> <Button>Accept</Button>  <Button>Decline</Button></td>
+            </tr>
+            <tr>
+              <td>Wedding 1</td>
+              <td>06/29/2017  6:30 pm</td>
+              <td>Lviv, Street 1, 22/2</td>
+              <td> <Button>Edit</Button>  <Button>Delete</Button></td>
+            </tr>
+            <tr>
+              <td>Wedding 1</td>
+              <td>06/29/2017  6:30 pm</td>
+              <td>Lviv, Street 1, 22/2</td>
+              <td> <Button>Edit</Button>  <Button>Delete</Button></td>
+            </tr>
+          </tbody>
         </Table>
         {this.props.children}      
       </div>
