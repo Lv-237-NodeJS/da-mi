845290c567483259c5e6df7dc040db39880e4f3e
yuliannakorol <yuliannakorol@gmail.com>
2017-06-13 14:56:05 +0300

    added indentation and removed unnecessary code in Home.js

--- app/src/containers/Home/Home.js
+++ app/src/containers/Home/Home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Grid, Row, Col, Button, PageHeader } from 'react-bootstrap';
-import { connect } from 'react-redux';
 import { Login } from '../../components';
 
 class Home extends React.Component {
@@ -8,22 +7,16 @@ class Home extends React.Component {
   render() {
     return (
       <Grid>
-          <Row className="show-grid">
-            <Col sm={12} md={8}>
-              <PageHeader className="text-center">Da-Mi</PageHeader>
+        <Row className="show-grid">
+          <Col sm={12} md={8}>
+            <PageHeader className="text-center">Da-Mi</PageHeader>
               <p>Some text about our project!!!</p>
-            </Col>
-            <Col sm={12} md={4}>
-              <Login />
-            </Col>
-          </Row>
+          </Col>
+          <Col sm={12} md={4}>
+            <Login />
+          </Col>
+        </Row>
       </Grid>
     );
   }
 }
-
-const mapStatetoProps = state => ({
-  isAuth: state.login.isAuth
-});
-
-export default connect(mapStatetoProps)(Home);
