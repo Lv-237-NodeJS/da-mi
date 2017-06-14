71cc6fe93ed5fe4f55ba7a6525982a5071bd66a4
yuliannakorol <yuliannakorol@gmail.com>
2017-06-14 14:03:04 +0300

    refactored dashboard component

--- app/src/containers/Dashboard/Dashboard.js
+++ app/src/containers/Dashboard/Dashboard.js
@@ -8,18 +8,26 @@ export default class Dashboard extends React.Component {
     return (
       <div>
         <h1>Dashboard</h1>
-        <Tab.Container id='left-tabs-example' defaultActiveKey='first'>
+        <Tab.Container id='left-tabs-example' defaultActiveKey={0}>
           <Row className='clearfix'>
             <Col sm={4}>
               <Nav bsStyle='pills' stacked>
-                  {['My Cabinet', 'My Events', 'Invitations', 'Create New Event'].map(
-                    (title, index) => <NavItem eventKey={index}> {title} </NavItem>)}
+                {[
+                  'My Cabinet',
+                  'My Events',
+                  'Invitations',
+                  'Create New Event'
+                  ].map((title, index) => <NavItem eventKey={index}> {title} </NavItem>)}
               </Nav>
             </Col>
             <Col sm={8}>
               <Tab.Content animation>
-                {['Profile Component will be here', <EventsList />, 'My invitations will be here', 'Create new Event component will be here'].map(
-                  (content, index) => <Tab.Pane eventKey={index}> {content} </Tab.Pane>)}
+                {[
+                  'Profile Component will be here',
+                  <EventsList />,
+                  'My invitations will be here',
+                  'Create new Event component will be here'
+                  ].map((content, index) => <Tab.Pane eventKey={index}> {content} </Tab.Pane>)}
               </Tab.Content>
             </Col>
           </Row>
