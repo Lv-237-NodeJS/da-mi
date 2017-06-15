7589486d9c9557d979b406b83b9b2ce7ccdd1aa2
yuliannakorol <yuliannakorol@gmail.com>
2017-06-15 16:00:46 +0300

    updated EventsList component and added events reducer

--- app/src/redux/reducer.js
+++ app/src/redux/reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import items from './Test';
 import login from './Login';
+import events from './EventsReducers';
 
 export default combineReducers({
   items,
   login,
+  events,
   routing: routerReducer
 });
