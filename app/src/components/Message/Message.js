ea6fe1e253cf0884288a49ed73f2b9ac312ca0f8
yuriy <borodatuu@gmail.com>
2017-06-15 19:23:45 +0300

    return"export default connect"

--- app/src/components/Message/Message.js
+++ app/src/components/Message/Message.js
@@ -8,35 +8,32 @@ import style from './Message.scss';
 class Message extends React.Component {
   hide = () => {
     this.props.actions.showModal(false);
-  }
+  };
 
   render() {
     return (
       <Modal
         show={this.props.show}
         onHide={this.hide}
-        id="modal-container">
+        id="modal-container"
+        className="modal-backdrop">
         <Modal.Header closeButton>
           <Modal.Title id="contained-modal-title-lg">
             <p>{this.props.message}</p>
           </Modal.Title>
-        </Modal.Header>    
+        </Modal.Header>     
       </Modal>
     );
   } 
 }
 
-const mapStatetoProps = (state, ownProps) => {
-  {
+const mapStatetoProps = (state, ownProps) => ({
     message: state.signup.message,
     show: state.signup.show  
-  };
-};
+});
 
-const mapDispatchToProps = dispatch => {
-  {
+const mapDispatchToProps = dispatch => ({
     actions: bindActionCreators(showActions, dispatch)
-  };
-};
+});
 
 export default connect(mapStatetoProps, mapDispatchToProps)(Message);
