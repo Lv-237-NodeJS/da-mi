bad1cb72d96544431b3b209210ab42c7338d584f
yuriy <borodatuu@gmail.com>
2017-06-16 16:17:50 +0300

    change name of message

--- app/src/helper/messages.js
+++ app/src/helper/messages.js
@@ -1,6 +1,6 @@
 const messages = {
-  signup: 'You have successfully signed up! For confirmation please visit your e-mail',
-  error: 'That email address is already in use',
+  successSignup: 'You have successfully signed up! For confirmation please visit your e-mail',
+  emailExistError: 'That email address is already in use',
   emailError: 'E-mail is not correct',
   passwordError: 'At least 6 characters, one (a-z), one (A-Z), one (0-9), one ($@$!%*?&)',
   confirmationError: 'Passwords do not match'
