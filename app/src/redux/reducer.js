da020a8f5ca0c3156fa79cbdaa6e283e78efe08a
yuliannakorol <yuliannakorol@gmail.com>
2017-06-16 13:00:08 +0300

    renamed reducer and  cleanup code

--- app/src/redux/reducer.js
+++ app/src/redux/reducer.js
@@ -2,13 +2,11 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 import items from './Test';
 import login from './Login';
-import event from './eventReducers';
-import events from './EventsReducers';
+import eventsList from './EventsReducers';
 
 export default combineReducers({
   items,
   login,
-  event,
-  events,
+  eventsList,
   routing: routerReducer
 });
