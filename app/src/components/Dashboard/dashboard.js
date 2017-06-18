e7989157f74fde2bab0a71fea610c6ea652686fd
yuliannakorol <yuliannakorol@gmail.com>
2017-06-19 01:02:46 +0300

    updated dashboard layout

---
+++ app/src/components/Dashboard/dashboard.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'dashboard-menu': {
+    'position': 'relative',
+    'left': [{ 'unit': 'px', 'value': 15 }],
+    'top': [{ 'unit': 'px', 'value': 15 }],
+    'display': 'inline'
+  }
+});
---
+++ app/src/containers/Events/eventsList.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'eventsList': {
+    'display': 'inline-block',
+    'width': [{ 'unit': 'px', 'value': 900 }]
+  }
+});
