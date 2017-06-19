cb3d0aa6efb98981dad717fd862e30c7f563fbf9
yuliannakorol <yuliannakorol@gmail.com>
2017-06-20 00:22:17 +0300

    updated home container

--- app/src/containers/Home/Home.js
+++ app/src/containers/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
-import { Login } from '../../components';
+import { Login, Message } from '../../components';
 
 export default class Home extends React.Component {
 
   render() {
     return (
       <Grid>
+        <Message />
         <Row className='show-grid'>
           <Col sm={12} md={8}>
             <PageHeader className='text-center'>Da-Mi</PageHeader>
