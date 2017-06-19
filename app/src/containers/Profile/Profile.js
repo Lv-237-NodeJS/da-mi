0e28e05bd4e19a46636fda6fee4f1241e6528f8b
yuliannakorol <yuliannakorol@gmail.com>
2017-06-19 22:29:46 +0300

    removed duplicated actions

--- app/src/containers/Profile/Profile.js
+++ app/src/containers/Profile/Profile.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-// import * as profileActions from '../../redux/ProfileReducers';
 
 class Profile extends React.Component {
-
-//   componentWillMount() {
-//     this.props.actions.retrieveProfile(this.props.userId);
-//   }
 
   render() {
     const profile = this.props.profile;
@@ -26,9 +20,5 @@ const mapStateToProps = state => ({
   profile: state.profile.profile,
   userId: state.login.userId
 });
-
-// const mapDispatchToProps = dispatch => ({
-//   actions: bindActionCreators(profileActions, dispatch)
-// });
 
 export default connect(mapStateToProps)(Profile);
