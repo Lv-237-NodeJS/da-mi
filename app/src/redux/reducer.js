be1528c55e67ec4bbce68122cd3095733f55c708
yuriy <borodatuu@gmail.com>
2017-06-23 00:23:04 +0300

    delete test element

--- app/src/redux/reducer.js
+++ app/src/redux/reducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
-import items from './Test';
 import signup from './Signup';
 import login from './Login';
 import event from './eventReducers';
