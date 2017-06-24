06e638f1955caf032d5e0264bcf14e03c48c8779
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-24 23:20:00 +0300

    Corrected RegExp for the date_event field"

--- app/src/containers/Newevent/Newevent.js
+++ app/src/containers/Newevent/Newevent.js
@@ -40,7 +40,7 @@ class Newevent extends React.Component {
   getValidationState = fieldName => {
     const pattern = {
       name: /^.{4,}$/,
-      date_event:  /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/
+      date_event:  /^(0?[1-9]|[12][0-9]|3[01])[/](0?[1-9]|1[012])[/]\d{4}$/
     };
 
     const newState = this.state;
