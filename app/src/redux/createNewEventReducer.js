ae127a8d4170b226b6cdf4ee526c3eec48ea59b4
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-06-24 20:28:47 +0300

    Added reducer and action for the Newevent component

---
+++ app/src/redux/createNewEventReducer.js
@@ -0,0 +1,74 @@
+import request from '../helper/request';
+import { API } from './../helper/constants';
+import { push } from 'react-router-redux';
+import { browserHistory } from 'react-router';
+const CREATE_NEW_EVENT_SUCCESS = 'CREATE_NEW_EVENT_SUCCESS';
+const CREATE_NEW_EVENT_FAILURE = 'CREATE_NEW_EVENT_FAILURE';
+const CREATE_NEW_EVENT_REQUEST = 'CREATE_NEW_EVENT_REQUEST';
+
+const initialState = {
+  isCreated: false,
+  illegalInput: true,
+  error: false
+};
+
+export default function createNewEventReducer(state = initialState, action) {
+  switch (action.type) {
+    case CREATE_NEW_EVENT_FAILURE:
+      return {
+        ...state,
+        error: true
+      };
+    case CREATE_NEW_EVENT_SUCCESS:
+      return {
+        ...state,
+        illegalInput: false,
+        isCreated: true
+      };
+    default:
+      return state;
+  }
+}
+
+export function createNewEventSuccess() {
+  browserHistory.push('/events');
+  return {
+    type: CREATE_NEW_EVENT_SUCCESS
+  };
+}
+
+export function createNewEventFailure() {
+  return {
+    type: CREATE_NEW_EVENT_FAILURE
+  };
+}
+
+export function createNewEventRequest() {
+  return {
+    type: CREATE_NEW_EVENT_REQUEST
+  };
+}
+
+export function createNewEvent(name, date_event, location_name, longitude, latitude, description) {
+  let event = {
+    name: name,
+    date_event: date_event,
+    location_name: location_name,
+    longitude: longitude,
+    latitude: latitude,
+    description: description
+  };
+  return dispatch => {
+    dispatch(createNewEventRequest());
+    request()
+      .post(API.HOST + API.PORT + '/api/events')
+      .send(event)
+      .end((err, res) => {
+        if (err || !res.ok) {
+          dispatch(createNewEventFailure());
+        } else {
+          dispatch(createNewEventSuccess());
+        }
+      });
+  };
+}
