8557a941bfaee9d6bb91479146bd0f98d476e7ec
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-06-26 12:51:05 +0300

    Integrated guests list into event details

--- app/src/redux/reducer.js
+++ app/src/redux/reducer.js
@@ -4,11 +4,13 @@ import signup from './Signup';
 import login from './Login';
 import event from './eventReducers';
 import eventsList from './EventsReducers';
+import invite from './Invite';
 
 export default combineReducers({
   login,
   event,
   eventsList,
   signup,
+  invite,
   routing: routerReducer
 });
