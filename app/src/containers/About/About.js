405254f667cd210e2aa1eb80390780ea38ff357d
yuriy <borodatuu@gmail.com>
2017-06-27 01:30:00 +0300

    add image and text to About

--- app/src/containers/About/About.js
+++ app/src/containers/About/About.js
@@ -1,44 +1,49 @@
 import React from 'react';
 import { Grid, Row, Col, PageHeader, Image } from 'react-bootstrap';
+import './About.scss';
 
 export default class About extends React.Component {
   render() {
-    const Names = {
-      'Maryana Svitlyk': require('../../../img/Maryana.svg'),
-      'Yulianna Korol': require('../../../img/Yulya.svg'),
-      'Ivan Yarymovych': require('../../../img/Vanja.svg'), 
-      'Andrii Voitovych': require('../../../img/Andriy.svg'),
-      'Denis Bodnar': require('../../../img/Denis.svg'),
-      'Ivan Shyyka': require('../../../img/Ivan.svg'),
-      'Yuriy Kokhalevych': require('../../../img/Yura.svg')
+    const names = {
+      'Maryana Svitlyk': 'Maryana',
+      'Yulianna Korol': 'Yulya',
+      'Ivan Yarymovych': 'Vanja', 
+      'Andrii Voitovych': 'Andriy',
+      'Denis Bodnar': 'Denis',
+      'Ivan Shyika': 'Ivan',
+      'Yuriy Kokhalevych': 'Yura'
     };
     return (
       <Grid>
         <Row className='show-grid'>
-          <Col xs={12} sm={12} md={12}>
+          <Col xs={6} sm={6} md={6} >
+            <div id='gift'className="container"></div>
+          </Col>
+          <Col xs={6} sm={6} md={6}>
             <PageHeader className='text-center'>Da-Mi</PageHeader>
-            <p>Do you plan to celebrate your birthday, wedding or other event 
-            and do not want to get 3 identical electric kettles as a gift? 
-            Then it's up to you! Our project will help you to get only the desired 
+            <p className='textAbout' xs={6} sm={4} md={3} >Do you plan to celebrate your birthday, wedding or other event 
+            and do not want to get 3 identical electric kettles as a gift &#63; 
+            Then it&#39;s up to you! </p> 
+            <p className='textAbout' xs={6} sm={4} md={3} >Our project will help you to get only the desired 
             gifts. Register and get the opportunity to create an event and send an 
             invitation to the guests. Make your list of gifts, and guests can 
-            choose any of the gifts you desire, buy it individually or join forces.</p>
+            choose any of the gifts you desire, buy it individually or join forces.
+            This will facilitate the process of choosing gifts for guests and help 
+            you get closer to your dream</p>
           </Col>
         </Row>
         <Row className='show-grid'>
           <PageHeader className='text-center'>Our Team</PageHeader>
-          {Object.keys(Names).map(param =>
-            <Col xs={3} sm={3} md={3}>
-              <Image src={Names[param]} thumbnail/>
-              <p>{param}</p>
-            </Col>
-          )}
-        </Row>
-        <Row className='show-grid'>
-          <p>We are a young, talented and fun team that creates a new and promising product. 
+          <p className='textAbout'>We are young, talented and fun team that creates a new and promising product. 
           We are evolving and changing to make it easier for you to plan events and make them 
           more memorable and impressive. We look forward to your comments and wishes.</p>
-        </Row>
+          {Object.keys(names).map(param =>
+            <Col xs={6} sm={4} md={3} key={param}>
+              <div id={names[param]} className="container"></div>
+              <p className='textTeam'>{param}</p>
+            </Col>
+          )}
+        </Row>        
       </Grid>
     );
   }
