8c559b25b943579c750f3879ed6b048624594381
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-06-26 17:36:58 +0300

    Integrated guests list into event details

--- app/src/containers/index.js
+++ app/src/containers/index.js
@@ -6,7 +6,6 @@ export Main from './Main/Main';
 export Maps from './Maps/Maps';
 export EventsList from './Events/EventsList';
 export EventDetails from './Event/EventDetails';
-export Guest from './Guest/sampleGuest';
 export Gift from './Gift/sampleGift';
 export Signup from './Signup/Signup';
 export Profile from './Profile/Profile';
