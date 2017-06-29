1cdb088a92ac10648e4c153c49a08301d41c05bb
yuliannakorol <yuliannakorol@gmail.com>
2017-06-29 13:15:41 +0300

    created Update Profile page view

--- app/src/containers/Profile/Profile.js
+++ app/src/containers/Profile/Profile.js
@@ -1,15 +1,103 @@
 import React from 'react';
+import { Row, Col, Image, FormGroup, ControlLabel, Form, FormControl, Button, ButtonToolbar,
+  Tabs, Tab  } from 'react-bootstrap';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/src/stylesheets/datepicker.scss';
+import './profile.scss';
+
+let FieldGroup = ({label, ...props}) => (
+  <div>
+    <FormGroup controlId='formControlsText'>
+      <ControlLabel>{label}</ControlLabel>
+      <FormControl {...props} />
+    </FormGroup>
+  </div>
+);
 
 class Profile extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      birthdate: null,
+      address: '',
+      city: '',
+      country: ''
+    };
+  }
+
+  handleChange = stateName => e => {
+    this.setState({[stateName]: e.target.value});
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.actions.updateProfile(this.state.param);
+  }
 
   render() {
     const { profile } = this.props;
+
+    const fieldsName = {
+      firstName: 'First Name',
+      lastName: 'Last Name',
+      birthdate: 'Birthdate',
+      address: 'Address',
+      city: 'City',
+      country: 'Country'
+    };
+
     return (
-      <div>
-        <h1>My Profile Info</h1>
-        <p>First Name: {profile.first_name}</p>
-        <p>Last Name: {profile.last_name}</p>
+      <div className='profile-details'>
+        <Col sm={8} className='container'>
+          <Tabs defaultActiveKey={1} id='uncontrolled-tab-example'>
+            <Tab eventKey={1} title='Profile Info'>
+              <h1>Edit Profile</h1>
+              <Row>
+                <Col  md={4} className='text-center'>
+                  <div className='no-avatar'></div>
+                  <h6>Upload a different photo</h6>
+                  <input type='file' className='form-control' />
+                </Col>
+              </Row>
+              
+              <Form horizontal onSubmit={this.handleSubmit}>
+                <Row>
+                  { Object.keys(fieldsName, profile).map(param => 
+                    <FieldGroup 
+                      key={param}
+                      label={fieldsName[param]}
+                      name={param}
+                      placeholder={fieldsName[param]}
+                      value={profile[param]}
+                      onChange={this.handleChange(param)}
+                    /> 
+                  )}
+                </Row>
+                <Row> 
+                  <hr/> 
+                  <ButtonToolbar>
+                    <Button type='submit' bsStyle='primary'>
+                      Save Changes
+                    </Button>
+                    <span> </span>
+                    <Button type='reset' bsStyle='default'>
+                      Cancel
+                    </Button>
+                  </ButtonToolbar>
+                </Row> 
+              </Form>
+            </Tab>
+            <Tab eventKey={2} title='Security Settings'>
+            </Tab>
+
+          </Tabs>
+        </Col>
       </div>
     );
   }
