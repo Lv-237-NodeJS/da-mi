3a05f11fb839239f53a3b9d581cb8d05dac8f10c
Maryana Svitlyk <svitlyk.m.r@gmail.com>
2017-07-03 18:26:56 +0300

    Fixed indentation

--- app/src/components/GuestsModalForm/GuestsModalForm.js
+++ app/src/components/GuestsModalForm/GuestsModalForm.js
@@ -2,11 +2,7 @@ import React from 'react';
 import { Button, Form, FormGroup, Col, ButtonToolbar } from 'react-bootstrap';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-<<<<<<< 9b7a3f40be421c92b72a5c489fcb00efc1fad036
 import * as inviteActions from '../../redux/inviteReducers';
-=======
-import * as inviteActions from '../../redux/invite';
->>>>>>> Added guests modal window
 
 const ListButton = ({...props}) => (
   <Button
@@ -35,21 +31,12 @@ class GuestsModalForm extends React.Component {
     };
   }
   
-<<<<<<< 9b7a3f40be421c92b72a5c489fcb00efc1fad036
   addEmail = () => {
     this.state.email &&
       this.setState({
         inputs: [...this.state.inputs, this.state.email],
         email: ''
       });
-=======
-  add = () => {
-  this.state.email &&
-    this.setState({
-      inputs: [...this.state.inputs, this.state.email],
-      email: ''
-    });
->>>>>>> Added guests modal window
   };
   
   setEmail = e => {
@@ -57,13 +44,8 @@ class GuestsModalForm extends React.Component {
   }
 
   handleChange = index => e => {
-<<<<<<< 9b7a3f40be421c92b72a5c489fcb00efc1fad036
     const newEmails = this.state.inputs.map((email, emailIndex) => (
       (index !== emailIndex) && email || e.target.value
-=======
-    const newEmails = this.state.inputs.map((email, j) => (
-      (index !== j) && email || e.target.value
->>>>>>> Added guests modal window
     ));
     this.setState({inputs: newEmails});
   }
@@ -74,11 +56,7 @@ class GuestsModalForm extends React.Component {
 
   deleteEmail = index => () => {
     this.setState({
-<<<<<<< 9b7a3f40be421c92b72a5c489fcb00efc1fad036
       inputs: this.state.inputs.filter((email, emailIndex) => index !== emailIndex)
-=======
-      inputs: this.state.inputs.filter((input, j) => index !== j)
->>>>>>> Added guests modal window
     });
   }
 
@@ -103,11 +81,7 @@ class GuestsModalForm extends React.Component {
             </Col>
             <Col xs={2}>
               <ListButton
-<<<<<<< 9b7a3f40be421c92b72a5c489fcb00efc1fad036
                 onClick={this.addEmail}
-=======
-                onClick={this.add}
->>>>>>> Added guests modal window
                 className='plus'
               />
             </Col>
@@ -125,11 +99,7 @@ class GuestsModalForm extends React.Component {
                   onChange={this.handleChange(index)}
                 />
               </Col>
-<<<<<<< 9b7a3f40be421c92b72a5c489fcb00efc1fad036
               <Col xs={4} sm={3} className='listItemBar'>
-=======
-              <Col xs={4}  sm={3} className='listItemBar'>
->>>>>>> Added guests modal window
                 <ButtonToolbar>
                   {['pencil', 'trash'].map(param =>
                     <ListButton
@@ -146,10 +116,6 @@ class GuestsModalForm extends React.Component {
           <Col xsOffset={4}>
             <Button
               type='submit'
-<<<<<<< 9b7a3f40be421c92b72a5c489fcb00efc1fad036
-=======
-              className='modal-save-button'
->>>>>>> Added guests modal window
               bsStyle='primary'
               bsSize='large'>Save</Button>
           </Col>
