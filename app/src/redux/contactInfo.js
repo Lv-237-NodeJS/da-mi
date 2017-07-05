97c119b8e95f320c8c96614cea0c35d57ce4a7b3
yuriy <borodatuu@gmail.com>
2017-07-06 02:52:11 +0300

    create redux for contact

---
+++ app/src/redux/contactInfo.js
@@ -0,0 +1,58 @@
+import request from '../helper/request';
+import { messages, API } from '../helper';
+import { browserHistory } from 'react-router';
+
+const SHOW_MODAL = 'SHOW_MODAL';
+const CONTACT_REQUEST = 'CONTACT_REQUEST';
+const MESSAGE_MODAL = 'MESSAGE_MODAL';
+
+export default function changeReducer(state = {show: false, message: ''}, action) {
+  switch (action.type) {
+    case SHOW_MODAL:
+      return Object.assign({}, state, {show: action.show});
+    case MESSAGE_MODAL:
+      return Object.assign({}, state, {message: action.message});
+    default:
+      return state;
+  }
+}
+
+export function showModal(show) {
+  return {
+    type: SHOW_MODAL,
+    show: show
+  };
+}
+
+export function messageModal(message) {
+  return {
+    type: MESSAGE_MODAL,
+    message: message
+  };
+}
+
+export function contactRequest() {
+  return {
+    type: CONTACT_REQUEST
+  };
+}
+
+export function contactInfo(name, surname, email, textarea) {
+  let data = {
+    name: name,
+    surname: surname,
+    email: email,
+    textarea: textarea
+  };
+  return dispatch => {
+    dispatch(contactRequest());
+    request()
+      .post(API.HOST + API.PORT + '/api/support')
+      .send(data)
+      .end(function(err, res) {
+        (res.status == 200) && dispatch(messageModal(messages.sendMessage)) &&
+        dispatch(showModal(true)) || dispatch(messageModal(err.response.text))&&
+        dispatch(showModal(true))
+      });
+  };
+}
