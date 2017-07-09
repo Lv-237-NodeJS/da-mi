ef2b686117314edd7c50e93b291b714b7936a7cd
Denis <denis.bodnar@gmail.com>
2017-07-10 02:34:23 +0300

    Add styles

--- app/src/containers/GiftList/GiftList.js
+++ app/src/containers/GiftList/GiftList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Accordion, Panel, Button, ButtonToolbar, Image } from 'react-bootstrap';
 import { EditGift } from 'src/containers';
+import './giftList.scss';
 
 export default class GiftList extends React.Component {
   render() {
@@ -11,11 +12,18 @@ export default class GiftList extends React.Component {
         this.props.actions.deleteGift(this.props.id, gift.id);
       };
 
+      const linkCheck = link => {
+        let protocol = link.slice(0, 4);
+        return ((protocol !== 'http') && 'http://') || '';
+      };
+
       return (
         <Panel header={gift.name} eventKey={gift.id} key={gift.id}>
           {!!gift.image && <Image src={gift.image} responsive/>}
-          <p>Description:&nbsp;{gift.description}</p>
-          <p>Link:&nbsp;<a href={'http://' + gift.link}>{gift.link}</a></p>
+          <p><span className='event-caption'>Description:</span>{gift.description}</p>
+          {!!gift.link && <p><span className='event-caption'>Link:</span>
+            <a href={`${linkCheck(gift.link)}${gift.link}`}>{gift.link}</a>
+          </p>}
           <ButtonToolbar>
             <EditGift id={this.props.id} gift={gift} actions={this.props.actions}/>
             <Button bsStyle='danger' bsSize='small' onClick={handleDelete}>
