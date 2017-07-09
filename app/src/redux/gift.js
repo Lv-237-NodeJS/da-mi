771f93654f22f43bf144e5d032eab0cd0d06e291
Denis <denis.bodnar@gmail.com>
2017-07-09 22:23:38 +0300

    Fix path and improve reducer

--- app/src/redux/gift.js
+++ app/src/redux/gift.js
@@ -1,5 +1,5 @@
-import { API } from './../helper/constants';
-import request from './../helper/request';
+import request from 'src/helper/request';
+import { API } from 'src/helper/constants';
 
 const FETCH_GIFTS_SUCCESS = 'FETCH_GIFTS_SUCCESS';
 const FETCH_GIFTS_FAIL = 'FETCH_GIFTS_FAIL';
@@ -10,75 +10,55 @@ const DELETE_GIFT_FAIL = 'DELETE_GIFT_FAIL';
 const UPDATE_GIFT_SUCCESS = 'UPDATE_GIFT_SUCCESS';
 const UPDATE_GIFT_FAIL = 'UPDATE_GIFT_FAIL';
 
-export const fetchGifts = eventId => {
-  return dispatch => {
-    return request()
-      .get(`${API.URL}/api/events/${eventId}/gifts`)
-      .end((err, res) => err &&
-          dispatch({
-            type: FETCH_GIFTS_FAIL,
-            payload: err
-          }) ||
-          dispatch({
-            type: FETCH_GIFTS_SUCCESS,
-            payload: res.body
-          })
-      );
-  };
-};
+export const fetchGifts = eventId => dispatch => request()
+  .get(`${API.URL}/api/events/${eventId}/gifts`)
+  .end((err, res) => err &&
+    dispatch({
+      type: FETCH_GIFTS_FAIL,
+      payload: err.response.body.message
+    }) ||
+    dispatch({
+      type: FETCH_GIFTS_SUCCESS,
+      payload: res.body
+    }));
 
-export const createGift = (eventId, gift) => {
-  return dispatch => {
-    return request()
-      .post(`${API.URL}/api/events/${eventId}/gifts`)
-      .send(gift)
-      .end((err, res) => err &&
-          dispatch({
-            type: CREATE_GIFT_FAIL,
-            payload: err
-          }) ||
-          dispatch({
-            type: CREATE_GIFT_SUCCESS,
-            payload: res.body
-          })
-      );
-  };
-};
+export const createGift = (eventId, gift) => dispatch => request()
+  .post(`${API.URL}/api/events/${eventId}/gifts`)
+  .send(gift)
+  .end((err, res) => err &&
+    dispatch({
+      type: CREATE_GIFT_FAIL,
+      payload: err.response.body.message
+    }) ||
+    dispatch({
+      type: CREATE_GIFT_SUCCESS,
+      payload: res.body
+    }));
 
-export const updateGift = (eventId, giftId, gift) => {
-  return dispatch => {
-    return request()
-      .put(`${API.URL}/api/event/${eventId}/gift/${giftId}`)
-      .send(gift)
-      .end((err, res) => err &&
-          dispatch({
-            type: UPDATE_GIFT_FAIL,
-            payload: err
-          }) ||
-          dispatch({
-            type: UPDATE_GIFT_SUCCESS,
-            payload: res.body
-          })
-      );
-  };
-};
+export const updateGift = (eventId, giftId, gift) => dispatch => request()
+  .put(`${API.URL}/api/event/${eventId}/gift/${giftId}`)
+  .send(gift)
+  .end((err, res) => err &&
+    dispatch({
+      type: UPDATE_GIFT_FAIL,
+      payload: err.response.body.message
+    }) ||
+    dispatch({
+      type: UPDATE_GIFT_SUCCESS,
+      payload: res.body
+    }));
 
-export const deleteGift = (eventId, giftId) => {
-  return dispatch => {
-    return request()
-      .delete(`${API.URL}/api/event/${eventId}/gift/${giftId}`)
-      .end((err, res) => err &&
-          dispatch({
-            type: DELETE_GIFT_FAIL,
-            payload: err
-          }) ||
-          dispatch({
-            type: DELETE_GIFT_SUCCESS,
-            payload: giftId
-          })
-      );
-  };
-};
+export const deleteGift = (eventId, giftId) => dispatch => request()
+  .delete(`${API.URL}/api/event/${eventId}/gift/${giftId}`)
+  .end((err, res) => err &&
+    dispatch({
+      type: DELETE_GIFT_FAIL,
+      payload: err.response.body.message
+    }) ||
+    dispatch({
+      type: DELETE_GIFT_SUCCESS,
+      payload: giftId
+    }));
 
 const initialState = {
   gifts: [],
