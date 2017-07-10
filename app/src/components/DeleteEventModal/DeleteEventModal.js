b7728cfc609b89a53092421e9add97fa3cb10d70
Ivan Yarymovych <ivan11yarymovych@gmail.com>
2017-07-11 00:10:41 +0300

    Added styles for the deleteEventModal component

--- app/src/components/DeleteEventModal/DeleteEventModal.js
+++ app/src/components/DeleteEventModal/DeleteEventModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
-import './DeleteEventModal';
+import './DeleteEventModal.scss';
 
 export default class DeleteEventModal extends React.Component {
   constructor(props) {
@@ -26,10 +26,11 @@ export default class DeleteEventModal extends React.Component {
           onHide={this.toggleModal}
           backdropClassName='modal-backdrop'>
           <Modal.Header className='deleteEventModalHeader' closeButton>
-            <Modal.Title>Are you sure that you want to delete this event?</Modal.Title>
+            <Modal.Title>Delete event</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Button bsStyle='danger' className='pull-left deleteEventModalYes' onClick={this.toggleModal}>
+            <h4 className='deleteEventModalBodyHeader'>Are you sure that you want to delete this event?</h4>
+            <Button bsStyle='danger' className='pull-left deleteEventModalYes'>
               Yes
             </Button>
             <Button bsStyle='success' className='pull-right deleteEventModalNo' onClick={this.toggleModal}>
