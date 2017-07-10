095ac4b8f3da6be4f56b569ea5feebb2d9470eea
Denis <denis.bodnar@gmail.com>
2017-07-10 20:00:49 +0300

    Fix warnings

--- app/src/containers/GiftList/GiftList.js
+++ app/src/containers/GiftList/GiftList.js
@@ -14,23 +14,23 @@ export default class GiftList extends React.Component {
 
       return (
         <Panel header={gift.name} eventKey={gift.id} key={gift.id}>
-        {!!gift.image &&
-          <div className='gift-image' style={{backgroundImage: `url(${gift.image})`}}>
-        </div>}
-        <div className='desc-block'> 
-          <p><span className='gift-caption'>Description:</span>{gift.description}</p>
-          {!!gift.link && <p><span className='gift-caption'>Link:</span>
-            <a href={gift.link} target='_blank'>link to present</a>
-          </p>}
-          <ButtonToolbar>
-            <EditGift id={this.props.id} gift={gift} actions={this.props.actions}/>
-            <Button bsStyle='danger' bsSize='small' onClick={handleDelete}>
-              Delete
-            </Button>
-            {gift.is_available === true &&
-            <Button bsStyle='success' bsSize='small'>Choose</Button>}
-          </ButtonToolbar>
-        </div>
+          {!!gift.image &&
+            <div className='gift-image' style={{backgroundImage: `url(${gift.image})`}}>
+          </div>}
+          <div className='desc-block'> 
+            <p><span className='gift-caption'>Description:</span>{gift.description}</p>
+              {!!gift.link && <p><span className='gift-caption'>Link:</span>
+              <a href={gift.link} target='_blank'>link to present</a>
+            </p>}
+            <ButtonToolbar>
+              <EditGift id={this.props.id} gift={gift} actions={this.props.actions}/>
+              <Button bsStyle='danger' bsSize='small' onClick={handleDelete}>
+                Delete
+              </Button>
+              {gift.is_available === true &&
+              <Button bsStyle='success' bsSize='small'>Choose</Button>}
+            </ButtonToolbar>
+          </div>
         </Panel>
       );
     });
