da7f4296d909facbd94312788a5670cd11475368
Denis <denis.bodnar@gmail.com>
2017-07-16 20:01:31 +0300

    Fix testing errors

--- test/Login_reducers.test.js
+++ test/Login_reducers.test.js
@@ -2,13 +2,20 @@ import loginReducer from './../app/src/components/Login/loginReducer';
 import * as loginActions from './../app/src/components/Login/loginActions';
 import * as router from 'react-router';
 
+const resolve = reducerFunctions => (state, action) => {
+  const reducer = reducerFunctions[action.type];
+  return reducer ? reducer(state, action) : state;
+};
+
+const login = resolve(loginReducer);
+
 describe('loginReducer execution', () => {
   const state = {};
   describe('LOGIN_USER_FAILURE', () => {
     it('should return the correct state', () => {
       const action = loginActions.loginUserFailure('message');
       
-      expect(loginReducer(state, action))
+      expect(login(state, action))
         .toEqual({
           ...state,
           illegalInput: true,
@@ -21,7 +28,7 @@ describe('loginReducer execution', () => {
     it('should return the correct state', () => {
       const action = loginActions.checkToken();
       
-      expect(loginReducer(state, action)).toEqual({
+      expect(login(state, action)).toEqual({
         ...state,
         isAuth: action.isAuth
       });
@@ -32,7 +39,7 @@ describe('loginReducer execution', () => {
     it('should return the correct state', () => {
       router.browserHistory = { push: () => {} };
       
-      expect(loginReducer(state, loginActions.logout())).toEqual({
+      expect(login(state, loginActions.logout())).toEqual({
         ...state,
         isAuth: false
       });
@@ -44,19 +51,12 @@ describe('loginReducer execution', () => {
       router.browserHistory = { push: () => {} };
       const action = loginActions.loginUserSuccess('token', 777);
       
-      expect(loginReducer(state, action)).toEqual({
+      expect(login(state, action)).toEqual({
         ...state,
         illegalInput: false,
         isAuth: true,
         userId: action.userId
       });
-    });
-  });
-
-  describe('default', () => {
-    it('should return the correct state', () => {
-      
-      expect(loginReducer(state, {})).toEqual(state);
     });
   });
 });
