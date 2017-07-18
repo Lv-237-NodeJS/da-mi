4856b0b7823495bce096dd1ee8b50c835bb8f229
yuriy <borodatuu@gmail.com>
2017-07-18 18:21:55 +0300

    add style to Home

--- app/src/containers/Home/Home.js
+++ app/src/containers/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Row, Col, PageHeader } from 'react-bootstrap';
 import { Login, Message } from 'src/components';
+import './Home.scss';
 
 export default class Home extends React.Component {
 
@@ -13,7 +14,7 @@ export default class Home extends React.Component {
             <PageHeader className='text-center'>Da-Mi</PageHeader>
             <p>Some text about our project!!!</p>
           </Col>
-          <Col sm={12} md={4}>
+          <Col sm={12} md={4} className='login'>
             <Login />
           </Col>
         </Row>
