d4f016671593c95a5953dddad6e460118bb6b368
yuliannakorol <yuliannakorol@gmail.com>
2017-07-21 10:06:28 +0300

    code cleanup

--- app/src/components/FileUploader/fileUploaderActions.js
+++ app/src/components/FileUploader/fileUploaderActions.js
@@ -3,14 +3,15 @@ import  { API, request }  from 'src/helper';
 const UPLOAD_FILE_SUCCESS = 'UPLOAD_FILE_SUCCESS';
 const UPLOAD_FILE_FAILURE = 'UPLOAD_FILE_FAILURE';
 
+const uploadProfileSuccess = res => ({
+  type: UPLOAD_FILE_SUCCESS,
+  payload: res.body
+});
+
 export const uploadFile = files => dispatch => 
   request()
     .post(`${API.URL}/api/upload`)
     .attach('fileToUpload', files[0])
     .end((error, res) => {
-      !error &&
-        dispatch({
-          type: UPLOAD_FILE_SUCCESS,
-          payload: res.body,
-        });
+      !error && dispatch(uploadProfileSuccess(res));
     });
