bc17ce094ddb1b8d40e1e60f3d1e71a694650ae0
yuliannakorol <yuliannakorol@gmail.com>
2017-07-26 12:25:53 +0300

    fixed warning in drop-down title

--- app/src/components/ProfileDropDown/ProfileDropDown.js
+++ app/src/components/ProfileDropDown/ProfileDropDown.js
@@ -8,6 +8,12 @@ import * as profileActions from 'src/containers/Profile/profileActions';
 import './profileDropDown.scss';
 
 class ProfileDropDown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: ''
+    };
+  }
 
   componentWillMount() {
     this.props.actions.profileActions.retrieveProfile(sessionStorage.getItem('userId'));
