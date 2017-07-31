8833bdb6f521df88f936c8ece58e5171ae2d2eb0
yuliannakorol <yuliannakorol@gmail.com>
2017-07-31 20:02:42 +0300

    changed home page background

--- app/src/containers/Home/Home.js
+++ app/src/containers/Home/Home.js
@@ -7,24 +7,26 @@ export default class Home extends React.Component {
 
   render() {
     return (
-      <Grid>
-        <Alerts />
-        <Row className='show-grid'>
-          <Col sm={12} md={8}>
-            <PageHeader className='text-center'>Da-Mi</PageHeader>
-            <div className='home-image-block'>
-              <div className='about-text'>
-                <h2>DA-MI - always get what you need</h2>
-                <p>service that helps people to invite 
-                  guests to their celebration and to order gifts they wish to get.</p>
-              </div>  
-            </div>
-          </Col>
-          <Col sm={12} md={4} className='login'>
-            <Login />
-          </Col>
-        </Row>
-      </Grid>
+      <div className='background-gift-image'>
+        <Grid>
+          <Alerts />
+          <Row className='show-grid background-block'>
+            <Col sm={12} md={8}>
+              <PageHeader className='text-center'>Da-Mi</PageHeader>
+              <div className='home-image-block'>
+                <div className='about-text'>
+                  <h2>DA-MI - always get what you need</h2>
+                  <p>service that helps people to invite 
+                    guests to their celebration and to order gifts they wish to get.</p>
+                </div>  
+              </div>
+            </Col>
+            <Col sm={12} md={4} className='login'>
+              <Login />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
